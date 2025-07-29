4c735bbc1edc4383175f4695f87fd4929f5bb5ae
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-07-29 23:24:49 +0600

    how to declare even-odd number using for-loop

---
+++ odd-even.js
@@ -0,0 +1,17 @@
+for (let i = 1; i <= 100; i++){
+    console.log(i);
+}
+
+// even 
+
+for (let i = 2; i <100; i=i+2){
+    console.log(i);
+}
+
+
+
+// odd
+
+for (let i = 1; i <100; i=i+2){
+    console.log(i);
+}
