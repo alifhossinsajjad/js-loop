e4a761f7459e42ce002d4d4aa1fca30d079dd83c
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-07-29 21:38:05 +0600

    how to use while loop

---
+++ while.js
@@ -0,0 +1,16 @@
+let num = 2;
+
+while (num <40){
+    console.log('I am soo Happy For That', num)
+    num = num + 1;
+    // num += 1;-------[shortcurt]
+    // num++;----------[shortcurt]
+}
+
+
+let money = 1 ;
+
+while (money <= 1000){
+    console.log('I have ', money)
+    money++;
+}
