bbec41770bd4067fb2d42eab8f304267d3d98b5e
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-07-29 23:36:58 +0600

    how to increment and decrement using for-loop

---
+++ decremental.js
@@ -0,0 +1,17 @@
+// increment
+
+for (let i = 1; i <=10; i++){
+    console.log(i);
+}
+
+
+// decrement
+
+
+for(let i = 10; i >= 0 ; i--){
+    console.log(i);
+}
+
+for(let i = 90; i >= 70 ; i--){
+    console.log(i);
+}
