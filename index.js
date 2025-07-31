c3fd0d0ce7d68ee75ca97549ec7d7f772bb688e2
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-07-31 17:56:34 +0600

    how to veryfi the index number

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const friends = ['alif', 'alif', 'saiful', 'arfan', 'midul', 'lifan', 'abir', 'rasibul', 'ankon', 'rafiul', 'romjan', 'sabbir', 'apon', 'alamin'];
+const friends = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
 
-console.log(friends.length);
+console.log(friends[4]);
